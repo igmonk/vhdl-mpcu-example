228ada8ca4d020e758b8b9a16d159431223baef1
unknown <imoiseyenko93@gmail.com>
2018-05-05 20:52:58 +0300

    MRAM component's other stuff.

--- fuseRelaunch.cmd
+++ fuseRelaunch.cmd
@@ -1,1 +1,1 @@
--intstyle "ise" -incremental -o "C:/Users/igor/Projects/ex1/MROM_TB_isim_beh.exe" -prj "C:/Users/igor/Projects/ex1/MROM_TB_beh.prj" "work.MROM_TB" 
+-intstyle "ise" -incremental -o "C:/Users/igor/Projects/ex1/MRAM_TB_isim_beh.exe" -prj "C:/Users/igor/Projects/ex1/MRAM_TB_beh.prj" "work.MRAM_TB" 
